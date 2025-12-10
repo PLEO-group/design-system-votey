1970db8223fc824a4d4b2cda05139d0dc3b7b400
Szymon <s.stawowy@pleodigital.com>
2025-12-10 15:08:40 +0100

    new publish

---
+++ dist/assets/react/icons/ui/IconUiArrowRight.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgIconUiArrowRight = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 26 26"
+    {...props}
+  >
+    <g id="icon_ui_arrow-right" clipPath="url(#a)">
+      <g id="icon_ui_arrow-right_2">
+        <g id="color_stroke">
+          <path
+            id="Vector"
+            fill="currentColor"
+            d="m24.48 12.657-6.483-6.644a.607.607 0 0 0-.868.848l5.4 5.532H1.935a.607.607 0 0 0 0 1.214h20.701l-5.659 5.522a.607.607 0 0 0 .848.868l6.643-6.482a.607.607 0 0 0 .01-.858"
+          />
+        </g>
+      </g>
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="currentColor" d="M0 0h26v26H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default SvgIconUiArrowRight;

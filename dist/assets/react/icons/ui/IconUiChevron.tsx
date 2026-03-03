f9724b9429419bfc8893f87a9737d50d4c361b12
Kamil Snopkowski <k.snopkowski@pleodigital.com>
2026-03-03 13:27:59 +0100

    updated icons

---
+++ dist/assets/react/icons/ui/IconUiChevron.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgIconUiChevron = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 14 9"
+    {...props}
+  >
+    <g id="icon_expand-arrow-1">
+      <g id="color_stroke">
+        <path
+          id="Vector"
+          fill="currentColor"
+          d="m1 1 6 4.698L13 1v2.302L7 8 1 3.302z"
+        />
+      </g>
+    </g>
+  </svg>
+);
+export default SvgIconUiChevron;
